01ae606180bb734559759d6223e5260a324e157f
Sujeet Kumar <sujeetkumar@microsoft.com>
2023-04-24 12:09:35 +0530

    stack prgrame

---
+++ Queue/CrudQueqe/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrudQueqe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Queqe Demo");
+            QueqeDemo obj=new QueqeDemo ();
+            obj.InserDataToQueqe();
+            obj.DisplayDataToQueqe();
+            obj.PickFirstElement();
+
+            obj.DeleteDataToQueqe();
+            obj.PickFirstElement();
+            obj.DisplayDataToQueqe();
+
+            Console.ReadLine();
+
+        }
+    }
+
+    public class QueqeDemo
+    {
+
+       Queue<int> queue;
+       public void InserDataToQueqe()
+        {
+            queue= new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+        }
+        public void DeleteDataToQueqe()
+        {
+           
+            if (queue.Count != 0)
+            {
+                var temp = queue.Peek();
+                queue.Dequeue();
+                Console.WriteLine($"Deleted Element : {temp}");
+            }
+        }
+
+        public void DisplayDataToQueqe()
+        {
+            foreach(var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void PickFirstElement()
+        {
+            var firstelement = queue.Peek();
+            Console.WriteLine($"First element : {firstelement}");
+        }
+    }
+}
---
+++ Stack/DeleteMiddleElememntFromStacks/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Reflection;
+
+namespace DeleteMiddleElememntFromStacks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stackdemo demo = new Stackdemo();
+            var stackoutput = demo.InsertValuesInStack();
+            demo.DisplayStack(stackoutput);
+            var stackafterdeletedmiddle = demo.DeleteMiddleFromStack(stackoutput);
+            demo.DisplayStack(stackafterdeletedmiddle);
+            Console.ReadLine();
+        }
+    }
+
+    class Stackdemo
+    {
+        Stack<int> stack = null;
+        public Stack<int> InsertValuesInStack()
+        {
+            stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            return stack;
+        }
+        public void DisplayStack(Stack<int> stack)
+        {
+            Console.WriteLine("Dispaly Stack Start:");
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("End Stack :");
+        }
+
+        public Stack<int> DeleteMiddleFromStack(Stack<int> stack)
+        {
+            Stack<int> tempsatck = new Stack<int>();
+            int midPos = (stack.Count / 2) + 1;
+            int count = stack.Count;
+            for(int i=1; i < count; i++)
+            {
+                if (i == midPos)
+                {
+                    stack.Pop();
+                    break;
+
+                }
+                tempsatck.Push(stack.Pop());
+            }
+
+            foreach (var item in tempsatck)
+            {
+                stack.Push(item);
+            }
+
+            return stack;
+        }
+
+
+        public Stack<int> DeleteMiddleFromStackWithouspace(Stack<int> stack)
+        {
+            Stack<int> tempsatck = new Stack<int>();
+            int midPos = (stack.Count / 2) + 1;
+            //int midPos = ceil((stack.Count + 1) / 2);
+            int count = stack.Count;
+            for (int i = 1; i < count; i++)
+            {
+                if (i == midPos)
+                {
+                    stack.Pop();
+                    break;
+
+                }
+                tempsatck.Push(stack.Pop());
+               
+            }
+
+            foreach (var item in tempsatck)
+            {
+                stack.Push(item);
+            }
+
+            return stack;
+        }
+    }
+}
