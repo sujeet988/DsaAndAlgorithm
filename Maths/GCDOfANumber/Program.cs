a924386e6618cb08acdf1dcca3afb8d64f3e6f68
Sujeet Kumar <sujeetkumar@microsoft.com>
2023-04-26 18:30:49 +0530

    GCD  ,LCM and Roatiion of a number

---
+++ Maths/GCDOfANumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GCDOfANumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
