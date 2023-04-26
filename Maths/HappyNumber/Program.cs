fe2b47a72160f2a94dd7d2b83d8cef8fb8d0021f
Sujeet Kumar <sujeetkumar@microsoft.com>
2023-04-26 20:03:54 +0530

    Happy Number

---
+++ Maths/HappyNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HappyNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
