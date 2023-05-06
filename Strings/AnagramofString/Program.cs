9e71ce6f1fc29c1ae44fb0c082cf7d19ca3898ba
Sujeet Kumar <sujeetkumar@microsoft.com>
2023-05-06 15:00:21 +0530

    Revert changes for anagram

--- Strings/AnagramofString/Program.cs
+++ Strings/AnagramofString/Program.cs
@@ -1,38 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace AnargramOfString
+namespace AnagramofString
 {
-    public class CheckStringISAnargram
+    internal class Program
     {
         /*
-        * q242 https://leetcode.com/problems/valid-anagram/
-        * An anagram of a string is another string that contains the same characters, only the order of characters can be different. For example, act and tac are an anagram of each other.
-        * https://takeuforward.org/data-structure/check-if-two-strings-are-anagrams-of-each-other/
-        */
+       * q242 https://leetcode.com/problems/valid-anagram/
+       * An anagram of a string is another string that contains the same characters, only the order of characters can be different. For example, act and tac are an anagram of each other.
+       * https://takeuforward.org/data-structure/check-if-two-strings-are-anagrams-of-each-other/
+       */
 
         // optimized approach
         // this  will handle both case  capital letter or small letter
         public static bool isAnagram(string a, string b)
         {
             //Your code here
-             int chardata= 256;
+            int chardata = 256;
             if (a.Length != b.Length)
             {
                 return false;
             }
-            int[] count=new int[chardata];
-            for(int i=0; i <a.Length; i++)
+            int[] count = new int[chardata];
+            for (int i = 0; i < a.Length; i++)
             {
                 count[a[i]]++; // increment charcater index value by 1 like a=97 so at index 97 value will increment by 1
                 count[b[i]]--; // decrement charcater index value by 1 like a=97 so at index 97 value will decrement by 1
             }
             //  check in count is at any index value is  greater than 0  or not. if any index value is greater than zero then not anagram
-            for(int i=0; i < count.Length; i++)
+            for (int i = 0; i < count.Length; i++)
             {
                 if (count[i] != 0)
                 {
@@ -78,5 +73,18 @@ namespace AnargramOfString
         }
         ////Time Complexity: O(n) where n is the length of string
         ////Space Complexity: O(1)
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anagram Demo");
+            string s1 = "aabca";
+            string s2 = "acaba";
+            Console.WriteLine("String first " + s1);
+            Console.WriteLine("String second " + s2);
+            bool isanagram = isAnagram(s1, s2);
+            Console.WriteLine("Is string is anagram : " + isanagram);
+            Console.ReadLine();
+        }
     }
 }
