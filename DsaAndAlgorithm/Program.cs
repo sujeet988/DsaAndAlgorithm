2cc8d0dbb9e4be30ae4a41183d0677ebf3074f85
Sujeet Kumar <sujeetkumar@microsoft.com>
2023-05-06 12:11:23 +0530

    QuestionList

--- DsaAndAlgorithm/Program.cs
+++ DsaAndAlgorithm/Program.cs
@@ -2,6 +2,14 @@
 
 namespace DsaAndAlgorithm
 {
+
+////1. Hcf 
+////2. LCM
+////3.Merge Sort
+////4 Quick Sort
+////5.Subset of string
+////6 permutaion of a string
+
     internal class Program
     {
         static void Main(string[] args)
