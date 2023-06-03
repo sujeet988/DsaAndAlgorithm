ffccf4f0bf98fe83078e86371f0e722441074530
Sujeet Kumar <sujeetkumar@microsoft.com>
2023-06-03 21:28:07 +0530

    secoondlargest Element

--- Arrays/SecondLargestElementInArray/Program.cs
+++ Arrays/SecondLargestElementInArray/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using SecondLargestElementInArray;
+using System.ComponentModel;
+using System.Globalization;
+using System.Xml.Linq;
+
+/*
+ * Input:
+N = 6
+Arr[] = { 12, 35, 1, 10, 34, 1}
+Output: 34
+Explanation: The largest element of the 
+array is 35 and the second largest element
+is 34.
+https://practice.geeksforgeeks.org/problems/second-largest3735/1
+ */
+int[] arr = new int[] { 12, 35, 1, 10, 34, 1 };
+
+Console.WriteLine("Find 2nd Largest Element in array");
+
+int n= SecondMaxElement.SecoondLargetElement(arr);
+Console.WriteLine(n); //34 ans
+Console.ReadLine();
+
+
+
---
+++ Arrays/SecondLargestElementInArray/SecondMaxElement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondLargestElementInArray
+{
+    public class SecondMaxElement
+    {
+        public static int SecoondLargetElement(int[] arr)
+        {
+            // There should be at least two elements
+            if (arr.Length < 2)
+            {
+                return -1;
+            }
+            int maxElement = int.MinValue;
+            int secondMaxElement = int.MinValue;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                // If current element is greater
+                // than maxElement then update both
+                // maxElement and maxElement
+
+
+                if (arr[i] > maxElement)
+                {
+                    secondMaxElement = maxElement;
+                    maxElement = arr[i];
+                }
+                // If arr[i] is in between maxElement
+                // and secondMaxElement then update secondMaxElement
+                else if (arr[i] > secondMaxElement && arr[i] != maxElement)
+                {
+                    secondMaxElement = arr[i];
+                }
+
+            }
+
+            //Handling worst case scenerio only need when execute on GFG OR leetcode
+            if (secondMaxElement == int.MinValue)
+            {
+                return -1;
+            }
+            return secondMaxElement;
+        }
+    }
+}
