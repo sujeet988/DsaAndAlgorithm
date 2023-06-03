6d53709a36eb12f0916e1a2a1c46d516a86fe6fa
Sujeet Kumar <sujeetkumar@microsoft.com>
2023-06-03 12:02:56 +0530

    array split by n number

---
+++ Arrays/DivideArrayDynamicPrint/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DivideArrayDynamicPrint
+{
+    internal class Program
+    {
+        /* Print array in 3 length.
+         * 10 20 30
+         * 40 50 60
+         * 70
+         */
+
+        public static void arraySplit(int[] arr,int maxlimit)
+        {
+            int count = 1;
+            for(int i=0; count <= maxlimit && i < arr.Length;i++)
+            {
+                if (count == maxlimit)
+                {
+                    Console.Write(arr[i]+" ");
+                    count =1;
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(arr[i] + " ");
+                    count++;
+                }
+               
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Divide Array Demo");
+            int[] arr=new int[7] { 10,20,30,40,50,60,70};
+            arraySplit(arr, 2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tree/BinaryTreeSk/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace BinaryTree
+{
+
+
+
+
+    public class BinaryTree
+    {
+        public  class Node
+        {
+           public int value;
+            public Node left;
+            public Node right;
+            public Node(int value)
+            {
+             this.value = value;
+            }
+        }
+
+        public Node root;
+
+        public void Populate()
+        {
+            Console.WriteLine("Enter the root node values");
+            int value=Convert.ToInt32(Console.ReadLine());
+            root=new Node(value);
+            Populate(root);
+        }
+
+        private void Populate(Node node)
+        {
+            Console.WriteLine("Do you want to enter left of " + node.value);
+            Boolean left=Convert.ToBoolean(Console.ReadLine());
+            if (left)
+            {
+                Console.WriteLine("Enter the value of left of " + node.value);
+                int value = Convert.ToInt32(Console.ReadLine());
+                node.left= new Node(value);
+                Populate(node.left);
+            }
+
+            Console.WriteLine("Do you want to enter right of " + node.value);
+            Boolean right = Convert.ToBoolean(Console.ReadLine());
+            if (right)
+            {
+                Console.WriteLine("Enter the value of right of " + node.value);
+                int value = Convert.ToInt32(Console.ReadLine());
+                node.right = new Node(value);
+                Populate(node.right);
+            }
+        }
+
+        public void Display()
+        {
+            Display(this.root, "");
+        }
+
+        private void Display(Node node, string indent)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            Console.WriteLine(indent + node.value);
+            Display(node.left, indent+"\t");
+            Display(node.right, indent+"\t");
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tree Demo");
+            BinaryTree obj=new BinaryTree();
+            obj.Populate();
+            obj.Display();
+            Console.ReadLine();
+
+        }
+    }
+}
