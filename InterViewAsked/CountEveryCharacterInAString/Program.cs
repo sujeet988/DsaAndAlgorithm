7088e64ffd080fb39fec35004e24f88fd0259077
Sujeet Kumar <sujeetkumar@microsoft.com>
2023-06-20 15:00:30 +0530

    Exam Code

---
+++ InterViewAsked/CountEveryCharacterInAString/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("To count the occurrences of each character in C#");
+string input = "Hello, WorLd!";
+
+string str=input.ToLower(); //l and L are different
+Dictionary<char,int> dic = new Dictionary<char,int>();
+foreach (char c in str)
+{
+    if (dic.ContainsKey(c))
+    {
+        dic[c] += 1;
+    }
+    else
+    {
+        dic.Add(c, 1);
+    }
+}
+
+foreach(var item in dic.Keys)
+{
+    Console.WriteLine("count of charcater : {0} is {1}", item, dic[item]);
+}
+Console.ReadLine();
