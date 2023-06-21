bf71fd6648a0483cad12a281a8a35bb9b2e9207c
Sujeet Kumar <sujeetkumar@microsoft.com>
2023-06-21 11:08:34 +0530

    KeyPAd Questionand Asnwer

---
+++ InterViewAsked/KeyPadRecursionQuestions/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DsaAndAlgorithm.InterViewAsked.KeyPadRecursionQuestions;
+Console.WriteLine("Letter Combinations of a Phone Number\r\n");
+////https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+/*
+ * Given a string containing digits from 2-9 inclusive, return all possible letter 
+combinations that the number could represent. Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons) is given below.
+Note that 1 does not map to any letters.
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+Input: digits = "2"
+Output: ["a","b","c"]
+ */
+
+string phoneNumber = "23";
+List<string> combinations = ProgramKeyPad.GetPhoneNumberCombinations(phoneNumber);
+
+Console.WriteLine("Possible combinations for " + phoneNumber + ":");
+foreach (string combination in combinations)
+{
+    Console.WriteLine(combination);
+}
---
+++ InterViewAsked/KeyPadRecursionQuestions/ProgramKeyPad.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsaAndAlgorithm.InterViewAsked.KeyPadRecursionQuestions
+{
+    public class ProgramKeyPad
+    {
+       public static string[] keypad = {
+        "0", "1", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ" };
+
+       public static List<string> GetPhoneNumberCombinations(string phoneNumber)
+        {
+   
+            List<string> combinations = new List<string>();
+            GenerateCombinations(phoneNumber, "", combinations, 0);
+            return combinations;
+
+        }
+        static void GenerateCombinations(string phoneNumber, string current, List<string> combinations, int index)
+        {
+            if (index == phoneNumber.Length)
+            {
+                combinations.Add(current);
+                return;
+            }
+
+            int digit = int.Parse(phoneNumber[index].ToString());
+            string letters = keypad[digit];
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                string next = current + letters[i];
+                GenerateCombinations(phoneNumber, next, combinations, index + 1);
+            }
+        }
+    }
+}
