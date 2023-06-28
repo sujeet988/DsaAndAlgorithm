291c7a0f1dcb821747a21e3d5ddb78eb4898bf46
Sujeet Kumar <sujeetkumar@microsoft.com>
2023-06-28 08:52:54 +0530

    skip characters

--- Recursion/SkipCharacters/Program.cs
+++ Recursion/SkipCharacters/Program.cs
@@ -16,8 +16,45 @@
             }
             else
             {
-                SkipCharacters(processed+ ch, unprocessed.Substring(1));
+                SkipCharacters(processed + ch, unprocessed.Substring(1));
             }
+        }
+
+        public static string SkipCharactersWithReturnsType(string processed, string unprocessed)
+        {
+            if (string.IsNullOrEmpty(unprocessed))
+            {
+                return processed;
+
+            }
+            char ch = unprocessed[0];
+            if (ch == 'e')
+            {
+                return SkipCharactersWithReturnsType(processed, unprocessed.Substring(1));
+            }
+            else
+            {
+                return SkipCharactersWithReturnsType(processed + ch, unprocessed.Substring(1));
+            }
+        }
+
+        public static string SkipCharactersWithReturnsTypeWithoutParamters(string unprocessed)
+        {
+            if (string.IsNullOrEmpty(unprocessed))
+            {
+                return " ";
+            }
+            char ch = unprocessed[0];
+
+            if (ch == 'e')
+            {
+                return SkipCharactersWithReturnsTypeWithoutParamters(unprocessed.Substring(1));
+            }
+            else
+            {
+              return  ch + SkipCharactersWithReturnsTypeWithoutParamters(unprocessed.Substring(1));
+            }
+
         }
 
         static void Main(string[] args)
@@ -26,6 +63,14 @@
             string name = "sujeet";
             Console.WriteLine("string after remove e from string ");
             SkipCharacters("", name); // output sujt
+
+            // skip characters using return type
+            var output = SkipCharactersWithReturnsType("", name);
+            Console.WriteLine(output);
+
+            //SkipCharactersWithReturnsTypeWithoutParamters
+            var result = SkipCharactersWithReturnsTypeWithoutParamters(name);
+            Console.WriteLine(result);
 
             Console.ReadLine();
         }
