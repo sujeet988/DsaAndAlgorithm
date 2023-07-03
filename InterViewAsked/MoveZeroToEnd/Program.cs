18a83cf1d74038a9bb7181446ea49650cbab4b5f
Sujeet Kumar <sujeetkumar@microsoft.com>
2023-07-03 10:01:01 +0530

    Reursion Notes

--- InterViewAsked/MoveZeroToEnd/Program.cs
+++ InterViewAsked/MoveZeroToEnd/Program.cs
@@ -8,7 +8,7 @@ namespace MoveZeroToEnd
     internal class Program
     {
         /*
-         Input :  arr[] = {1, 2, 0, 4, 3, 0, 5, 0};
+         * Input :  arr[] = {1, 2, 0, 4, 3, 0, 5, 0};
          Output : arr[] = {1, 2, 4, 3, 5, 0, 0, 0};
        */
 
