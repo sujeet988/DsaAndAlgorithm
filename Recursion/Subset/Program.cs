c79f19af030d98ee99533a1a07070c36a131a065
Sujeet Kumar <sujeetkumar@microsoft.com>
2023-07-03 16:44:14 +0530

    subset using ilist and recursion

--- Recursion/Subset/Program.cs
+++ Recursion/Subset/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace DsaAndAlgorithm.Recursion.Subset
+﻿using System.Net.Http.Headers;
+
+namespace DsaAndAlgorithm.Recursion.Subset
 {
     internal class Program
     {
@@ -22,6 +24,36 @@
             return subsets;
         }
 
+        ////public static void SubsetUsingListofList(List<int> processed,int[] unprocessed, int index, List<List<int>> ans)
+        ////{
+        ////    if (index >=unprocessed.Length)
+        ////    {
+        ////        ans.Add(processed);
+        ////        return;
+        ////    }
+        ////    // Exclude the current element
+        ////    SubsetUsingListofList(processed, unprocessed, index + 1, ans);
+        ////    // Include the current element
+        ////    int item = unprocessed[index];
+        ////    processed.Add(item);
+        ////    SubsetUsingListofList(processed, unprocessed,index+1, ans); ///// Include the current item
+        ////}
+
+        public static void SubsetUsingListofList(List<int> processed, int[] unprocessed, int index, IList<IList<int>> ans)
+        {
+            if (index >= unprocessed.Length)
+            {
+                ans.Add(processed);
+                return;
+            }
+            // Exclude the current element
+            SubsetUsingListofList(processed, unprocessed, index + 1, ans);
+            // Include the current element
+            int item = unprocessed[index];
+            processed.Add(item);
+            SubsetUsingListofList(processed, unprocessed, index + 1, ans); ///// Include the current item
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Subset Demo using loop/Iteration");
@@ -36,7 +68,19 @@
                 Console.WriteLine();
             }
 
-
+            Console.WriteLine("Subset Demo using Recursion");
+            IList<IList<int>> ans=new List<IList<int>>();
+            List<int> processed=new List<int>();
+            int index = 0;
+            SubsetUsingListofList(processed,arr, index, ans);
+            foreach (List<int> list in output)
+            {
+                foreach (var data in list)
+                {
+                    Console.Write(data);
+                }
+                Console.WriteLine();
+            }
             Console.ReadLine();
         }
     }
