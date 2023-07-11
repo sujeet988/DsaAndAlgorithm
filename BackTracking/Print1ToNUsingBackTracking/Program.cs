70d6e93a716549fb5c2594a05caf02e48e6ebf80
Sujeet Kumar <sujeetkumar@microsoft.com>
2023-07-11 20:52:11 +0530

    Print1ToN using backtracking

--- BackTracking/Print1ToNUsingBackTracking/Program.cs
+++ BackTracking/Print1ToNUsingBackTracking/Program.cs
@@ -5,9 +5,26 @@
         /*
          * https://www.linkedin.com/posts/shubham-upadhyay1_striver-recursion-notes-activity-7045627239131942912-AIRG/
          */
+
+        static void Print1ToN(int n)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+
+            Print1ToN(n - 1);
+            Console.WriteLine(n);
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Print 1 To N using Backtracking ");
+            Console.WriteLine("Enter number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Print1ToN(n);
+            Console.ReadLine();
         }
     }
 }
