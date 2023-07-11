9a444488dcc72b2b64bb5c9ec4778f7c85f1130b
Sujeet Kumar <sujeetkumar@microsoft.com>
2023-07-11 21:22:40 +0530

    SortAnArrayUsingRecursion

---
+++ Av/SortAnArrayUsingRecursion/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace SortAnArrayUsingRecursion
+{
+    internal class Program
+    {
+        public static void Sort(List<int> array)
+        {
+            if (array.Count == 1)
+            {
+                return;
+            }
+
+            int temp= array[array.Count-1];
+            array.RemoveAt(array.Count-1);
+            Sort(array);
+            Insert(array,temp);
+        }
+
+
+        public static void Insert(List<int> array, int temp)
+        {
+            if(array.Count==0 || array[array.Count - 1] <= temp)
+            {
+                array.Add(temp);
+                return;
+            }
+            int val= array[array.Count-1];
+            array.RemoveAt(array.Count - 1);
+            Insert(array, temp);
+            array.Add(val);
+          
+           
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sort An Array Using Recursion");
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(5);
+            list.Add(0);
+            list.Add(100);
+            Sort(list);
+            foreach(var item in list)
+            {
+                Console.WriteLine($"{item.ToString()}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
