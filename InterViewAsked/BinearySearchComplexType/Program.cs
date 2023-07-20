10a2d572461e05b7f936b56faac2bd03cf96ef53
Sujeet Kumar <sujeetkumar@microsoft.com>
2023-07-20 15:27:47 +0530

    BinearySearchComplexType

---
+++ InterViewAsked/BinearySearchComplexType/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DsaAndAlgorithm.InterViewAsked.BinearySearchComplexType
+{
+    internal class Program
+    {
+        public class OrderDetails
+        {
+            public List<int> OrderIds;
+            public DateTime OrderDate { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Search Complex type demo");
+            OrderDetails orderDetails = new OrderDetails();
+            orderDetails.OrderIds.
+        }
+    }
+}
