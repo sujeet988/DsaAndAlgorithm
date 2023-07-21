bee2cf7d52a21d6d607f93ec47b2990ab1a87ec3
Sujeet Kumar <sujeetkumar@microsoft.com>
2023-07-21 15:21:13 +0530

    CeilingOfANumberByBinarySearch

--- BinarySearch/CeilingOfANumber/Program.cs
+++ BinarySearch/CeilingOfANumber/Program.cs
@@ -28,13 +28,42 @@ namespace DsaAndAlgorithm.BinarySearch.CeilingOfANumber
             }
             return start;
         }
+
+        public static int CeilingOfANumberByBinarySearchApproach2(int[] arr, int target)
+        {
+            int start = 0;
+            int end = arr.Length - 1;
+            int result = -1;
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                if (arr[mid] == target)
+                {
+                    return mid;
+                }
+                else if (target < arr[mid])
+                {
+                    result = mid;
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+            return result;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Floor of a number using Binary Search");
+            Console.WriteLine("Celiling of a number using Binary Search");
             int[] arr = new int[7] { 2, 3, 5, 9, 14, 16, 18 };
             int target = 4;
             var output=  CeilingOfANumberByBinarySearch(arr, target);
-            Console.WriteLine("Celiling of  a number is "+ output); //3 is the largest number that is less then target=4 element
+            Console.WriteLine("index of Celiling of  a number is "+ output); //5 is the largest number then 4 so ans is index 2
+
+            var output2 = CeilingOfANumberByBinarySearchApproach2(arr, target);
+            Console.WriteLine("index of Celiling of  a number is " + output2); //5 is the largest number then 4  ans is index 2
+
             Console.ReadLine();
 
         }
