87233dd116469a9ea76c5471c380bdb23e47dba1
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-02-11 21:34:29 +0530

    Linked List Operation

---
+++ LinkedList/LinkedListAllOperation/LinkedList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListAllOperation
+{
+   public  class Node
+    {
+        public int data;
+        public Node next;
+        public Node()
+        {
+            next = null;
+
+        }
+    }
+
+
+    public class LinkedList
+    {
+        Node head;
+
+        public LinkedList()
+        {
+            head = null;
+        }
+
+        public bool IsEmpty()
+        {
+            if(head==null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+
+        public void InsertAtLast()
+        {
+
+        }
+
+        public void InsertAtFirst(int x)
+        {
+            Node newnode=new Node ();
+            newnode.data = x;
+            if(IsEmpty()) 
+            { 
+                head = newnode;
+
+            }
+        }
+
+        public  void DisplayNode()
+        {
+
+            Node temp = head;
+            while(temp != null)
+            {
+                Console.Write(temp.data+"->");
+                temp = temp.next;
+            }
+            Console.WriteLine("null ");
+
+        }
+        
+    }
+}
--- LinkedList/LinkedListAllOperation/Program.cs
+++ LinkedList/LinkedListAllOperation/Program.cs
@@ -1,51 +1,18 @@
 ﻿namespace LinkedListAllOperation
 {
 
-    class Node
-    {
-        public int data;
-        public Node next;
-        public Node(int data)
-        {
-            this.data = data;
-            next = null;
-
-        }
-
-        public static void DisplayNodeValue(Node head) 
-        {
-            Node temp=head;
-            while(temp != null)
-            {
-                Console.Write(temp.data);
-                if(temp.next != null)
-                {
-                    Console.Write("->");
-                }
-
-                temp=temp.next;
-            }
-        
-        }
-    }
-
 
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Linked List Operation");
-            Node head=new Node(1);
-            Node node2 = new Node(2);
-            head.next = node2;
-            Node node3 = new Node(3);
-            node2.next = node3;
-            Node node4 = new Node(4);
-            node3.next = node4;
-
-            // Display linked Values
-            Node.DisplayNodeValue(head);
-
+            LinkedList obj=new LinkedList();
+            for(int i = 0;i < 5; i++)
+            {
+                obj.InsertAtFirst(i);
+            }
+            obj.DisplayNode();
             Console.ReadLine(); 
 
         }
