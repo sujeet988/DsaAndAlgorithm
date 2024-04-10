82ed7d847d5b5e1bcb021baba18a3067e4779ecd
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-04-10 11:24:08 +0530

    added Eductaive Question

---
+++ Stack/MinFunctionUsingaStack/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MinFunctionUsingaStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
