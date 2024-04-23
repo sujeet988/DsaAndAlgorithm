362807a8470d8c3c16fc185433d5552f01c76297
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-04-23 17:56:25 +0530

    Interview Asked added

---
+++ CelebrityProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CelebrityProblem
+{
+    /*
+     * Intrerview asked
+     * https://www.geeksforgeeks.org/the-celebrity-problem/
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
