916f7050366a7e4251cfabca24722fb5d7a08e79
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-06-18 22:32:42 +0530

    DSA Questions

---
+++ InterViewAsked/SwappingpairsMakeSumEqual/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SwappingpairsMakeSumEqual
+{
+    /*
+     * https://www.geeksforgeeks.org/problems/swapping-pairs-make-sum-equal4142/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
+     * Given two arrays of integers a[] and b[] of size n and m, the task is to check if a pair of values (one value from each array) exists such that swapping the elements of the pair will make the sum of two arrays equal.
+
+Note: Return 1 if there exists any such pair otherwise return -1.
+    Input: n = 6, m = 4, a[] = {4, 1, 2, 1, 1, 2}, b[] = (3, 6, 3, 3)
+Output: 1
+Explanation: Sum of elements in a[] = 11, Sum of elements in b[] = 15, To get same sum from both arrays, we can swap following values: 1 from a[] and 3 from b[]
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
