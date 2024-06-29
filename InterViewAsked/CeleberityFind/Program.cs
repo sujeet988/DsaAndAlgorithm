4b191a2548f229a3328b1bd6dcf087cc9e42c7b0
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-06-29 10:33:39 +0530

    Celerbrity Problem

---
+++ InterViewAsked/CeleberityFind/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CeleberityFind
+{
+    /*
+     * https://www.geeksforgeeks.org/problems/the-celebrity-problem/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Celebrity Problem");
+        }
+    }
+}
---
+++ InterViewAsked/RottenOrange/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace RottenOrange
+{
+    /*
+     * 994 https://leetcode.com/problems/rotting-oranges/description/
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
