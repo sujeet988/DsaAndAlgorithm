50a1ec261a1d03203d21145b885b6fd13b99fcfc
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-06-29 10:36:56 +0530

    Added All Interview  Questions

---
+++ InterViewAsked/HouseRobber/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HouseRobber
+{
+    /*
+     * 198 https://leetcode.com/problems/house-robber/description/
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ InterViewAsked/SankesAndLadder/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SankesAndLadder
+{
+    /*
+     * https://leetcode.com/problems/snakes-and-ladders/description/
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("snakes-and-ladders ");
+        }
+
+    }
+
+}
