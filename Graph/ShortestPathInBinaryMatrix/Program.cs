b9236d9203f485d936892fd7b1dcbede0ec54bbe
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-07-05 22:12:45 +0530

    Notes Added

--- Graph/ShortestPathInBinaryMatrix/Program.cs
+++ Graph/ShortestPathInBinaryMatrix/Program.cs
@@ -1,5 +1,8 @@
 ﻿namespace ShortestPathInBinaryMatrix
 {
+    /*
+     * https://leetcode.com/problems/shortest-path-in-binary-matrix/description/
+     */
     internal class Program
     {
         static void Main(string[] args)
--- LinearSearch/ConsoleApp15/Program.cs
+++ LinearSearch/ConsoleApp15/Program.cs
@@ -2,11 +2,36 @@
 
 namespace ConsoleApp15
 {
+    /*
+     * https://www.geeksforgeeks.org/linear-search/
+     */
     internal class Program
     {
-        static void Main(string[] args)
+        public static int search(int[] arr, int N, int x)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < N; i++)
+            {
+                if (arr[i] == x)
+                    return i;
+            }
+            return -1;
+        }
+
+        // Driver's code
+        public static void Main()
+        {
+            int[] arr = { 2, 3, 4, 10, 40 };
+            int x = 10;
+
+            // Function call
+            int result = search(arr, arr.Length, x);
+            if (result == -1)
+                Console.WriteLine(
+                    "Element is not present in array");
+            else
+                Console.WriteLine("Element is present at index "
+                                  + result); // ans : 3
+            Console.ReadLine();
         }
     }
 }
