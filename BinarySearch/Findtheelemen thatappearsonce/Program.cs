1dcdf2d06af1e35a22e90984a6c803f5e22db8e7
DELL <DELL@DESKTOP-VLVF8CL>
2024-08-04 12:26:31 +0530

    Binary Search

---
+++ BinarySearch/Findtheelemen thatappearsonce/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Findtheelemen_thatappearsonce
+{
+    internal class Program
+    {
+        /*
+         * https://www.geeksforgeeks.org/problems/element-appearing-once2552/1?utm_source=youtube&utm_medium=collab_codein10_description&utm_campaign=element-appearing-once
+         * Given a sorted array arr[] of n positive integers having all the numbers occurring exactly twice, except for one number which will occur only once. Find the number occurring only once.
+
+            Examples :
+
+            Input: n = 5, arr[] = {1, 1, 2, 5, 5}
+            Output: 2
+            Explanation: Since 2 occurs once, while other numbers occur twice, 2 is the answer.
+            Input: n = 7, arr[] = {2, 2, 5, 5, 20, 30, 30}
+            Output: 20
+            Explanation: Since 20 occurs once, while other numbers occur twice, 20 is the answer.
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ BinarySearch/SearchinRotatedArray2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SearchinRotatedArray2
+{
+    internal class Program
+    {
+        /*
+         * https://www.geeksforgeeks.org/problems/search-in-rotated-array-2/1
+         * Given a sorted and rotated array arr of n elements and a target key which is rotated at some point, and may contain duplicates and given an element key. Check whether the key is present in array A or not.
+
+            Example 1:
+
+            Input:
+            n = 7
+            arr[] = {2,5,6,0,0,1,2}
+            key = 0
+            Output:
+            1
+            Explanation:
+            0 is found at index 3.
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Search in Rotated Array 2\r\n");
+            Console.ReadLine();
+        }
+    }
+}
