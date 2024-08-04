94b98e112d6c842ee240489346aef4570ae608d2
DELL <DELL@DESKTOP-VLVF8CL>
2024-08-04 14:23:36 +0530

    Double ended queue

---
+++ Queue/Dequeue/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+namespace DequeueDemo
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Double ended queue");
+           
+
+
+        }
+    }
+}
