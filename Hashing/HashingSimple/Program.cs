f8121553add1c03b11cd38b2256d4c15cf963bed
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-08-08 09:48:07 +0530

    Hashing Simple

---
+++ Hashing/HashingSimple/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HashingSimple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hashing Demo");
+            string name = "sujeet";
+            int code= name.GetHashCode(); //  system defined function
+            Console.WriteLine(code);
+            Console.ReadLine();
+        }
+    }
+}
