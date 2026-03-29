f764b079d10c62251292fcd94c9a3b6b75feb24f
sujeet kumar <sujeetkumar2010@live.com>
2026-03-29 16:00:55 +0530

    LinkedList Reverse

---
+++ LinkedList/LinkedListCycle/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkedListCycle
+{
+    internal class Program
+    {
+        /*
+         * https://neetcode.io/problems/linked-list-cycle-detection/question?list=neetcode150
+         * 
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ LinkedList/ReverseLinkedList/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ReverseLinkedList
+{
+    internal class Program
+    {
+        /*
+         * https://neetcode.io/problems/reverse-a-linked-list/question?list=neetcode150
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
