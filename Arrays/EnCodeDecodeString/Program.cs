878609a5aa841b2755e17222a02562ac9778cee0
sujeet kumar <sujeetkumar2010@live.com>
2026-04-02 16:41:01 +0530

    Encodde and decode string

---
+++ Arrays/EnCodeDecodeString/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace EnCodeDecodeString
+{
+    internal class Program
+    {
+        /*
+         * https://neetcode.io/problems/string-encode-and-decode/question
+        */
+        public static string Encode(IList<string> str) 
+        {
+            return "";
+        }
+
+        public static List<string> Decode(string str)
+        {
+            List<string> result = new List<string>();
+
+            return result;
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Encode and Decode Demo");
+
+            List<string> inputs = new List<string>();
+            inputs.Add("Hello");
+            inputs.Add("world");
+            var endcoderesult = Program.Encode(inputs);
+
+            var decodederesult = Program.Decode(endcoderesult);
+            foreach (var input in decodederesult)
+            {
+                Console.WriteLine(input);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
