91cfca08d626b466ee3aae1a4aebf85dc0310e94
sujeet kumar <sujeetkumar2010@live.com>
2026-04-03 13:59:39 +0530

    Removed count variable

--- TwoPointer/ContainerWIthMostWater/Program.cs
+++ TwoPointer/ContainerWIthMostWater/Program.cs
@@ -13,7 +13,6 @@
         public static int MaxAreaBruteForce(int[] height)
         {
             int max = 0;
-            int count = 0;
 
             for (int i = 0; i < height.Length; i++)
             {
