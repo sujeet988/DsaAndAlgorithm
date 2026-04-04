c47ff05fa1586c2f5a1043d0777e39d291e2fd35
sujeet kumar <sujeetkumar2010@live.com>
2026-04-04 08:24:11 +0530

    jagged array

---
+++ Overlapping/MergeInterval/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace MergeInterval
+{
+    internal class Program
+    {
+        /*
+         * https://leetcode.com/problems/merge-intervals/description/
+         * 
+         */
+
+        public static int[][] Merge(int[][] intervals)
+        {
+            int[][] arr = new int[2][];
+
+            return arr;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merge Interval Demo");
+
+            int[][] intervals = new int[4][];
+            intervals[0] = new int[2] { 1,3};
+            intervals[1] = new int[2]{ 2,6};
+            intervals[2] = new int[2] { 8, 10 };
+            intervals[3] = new int[2] { 15, 18 };
+
+            for (int i = 0; i < intervals.Length; i++)
+            {
+                for(int j =0; j < intervals[i].GetLength(0); j++)
+                {
+                    Console.Write(intervals[i][j]);
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
