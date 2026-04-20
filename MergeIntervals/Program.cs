d827f33bd7e6414237798bc09caa7c3f32e270f5
sujeet kumar <sujeetkumar2010@live.com>
2026-04-20 07:35:28 +0530

    Merge interval demo

---
+++ MergeIntervals/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MergeIntervals
+{
+    internal class Program
+    {
+
+        /*
+         * https://leetcode.com/problems/merge-intervals/
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merge Interval Demo");
+
+            Console.ReadLine();
+        }
+    }
+}
